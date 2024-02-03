73ea5e4527eba05f33b90c46acf3496f8b6b147b
keniour <el.roussarie@gmail.com>
2024-02-03 02:44:14 +0100

    ..

--- IMDB-review_dataset.py
+++ IMDB-review_dataset.py
@@ -55,7 +55,7 @@ partial_y_train=y_train[10000:]
 
 history=model.fit(partial_x_train,
                   partial_y_train,
-                  epochs=20,
+                  epochs=4,
                   batch_size=512,
                   validation_data=(x_val,y_val))
 
@@ -123,14 +123,29 @@ and so on.
  Try using the tanh activation (an activation that was popular in the early days of
 neural networks) instead of relu
 """
-
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import imdb
 from keras import models
 from keras import layers
 
+(train_data, train_labels),(test_data,test_labels)=imdb.load_data(num_words=10000)
+def vectorize_sequences(sequences,dimension=10000):
+    results=np.zeros((len(sequences),dimension))
+    for i, sequence in enumerate(sequences):
+        results[i][sequence]=1.
+    return results
+x_train = vectorize_sequences(train_data)
+x_test=vectorize_sequences(test_data)
+#vectorisation des labels
+y_train=np.asarray(train_labels).astype('float32')
+y_test=np.asarray(test_labels).astype('float32')
+
+
 model=models.Sequential()
-model.add(layers.Dense(64,activation='relu',input_shape=(10000,)))
-model.add(layers.Dense(64,activation='relu'))
-model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(64,activation='tanh',input_shape=(10000,)))
+#model.add(layers.Dense(64,activation='tanh'))
+#model.add(layers.Dense(64,activation='relu'))
 model.add(layers.Dense(1,activation='sigmoid'))
 
 
@@ -151,7 +166,7 @@ partial_y_train=y_train[10000:]
 
 history=model.fit(partial_x_train,
                   partial_y_train,
-                  epochs=20,
+                  epochs=5,
                   batch_size=512,
                   validation_data=(x_val,y_val))
 results=model.evaluate(x_test,y_test)
