b32c0e7808d6c4a809ebc7058f759d6b47f07a6e
keniour <el.roussarie@gmail.com>
2024-03-30 19:26:51 +0100

    end of the housing price prediction

--- Housing_price_prediction(3.6).py
+++ Housing_price_prediction(3.6).py
@@ -1,3 +1,4 @@
+#%%
 from keras.datasets import boston_housing
 import numpy as np
 (train_data, train_targets), (test_data, test_targets) =boston_housing.load_data()
@@ -21,4 +22,77 @@ def build_model():
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return model
 
-#reprendre au point 3.6.4
+#3.6.4 K-fold validation :
+    """Pour ajuster les parametres du modèles quand on a peut de paramètres 
+
+    Returns:
+        str: expl
+    """
+
+
+
+import numpy as np
+
+k=4
+num_val_samples=len(train_data)//k
+num_epochs=500
+all_scores=[]
+all_mae_histories=[]
+for i in range(k):
+    print('processing fold #', i)
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples] #prepares the validation data
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples] #
+    
+    partial_train_data = np.concatenate( #prepares the training data from all other partitions 
+        [train_data[:i * num_val_samples],
+        train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+        train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    
+    model = build_model()
+    history = model.fit(partial_train_data, partial_train_targets,
+                        validation_data=(val_data, val_targets),
+                        epochs=num_epochs, batch_size=1, verbose=0)
+    mae_history = history.history['val_mean_absolute_error']
+    all_mae_histories.append(mae_history)
+
+average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+
+
+
+def smooth_curve(points, factor=0.9):
+    smoothed_points=[]
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+        return smoothed_points
+smooth_mae_history = smooth_curve(average_mae_history[10:])
+
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+
+#TRAIN THE FINAL MODEL 
+
+model = build_model() #Trains it on the entirety of the data
+model.fit(train_data, train_targets, #Trains it on the entirety of the data 
+epochs=80, batch_size=16, verbose=0)
+test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+
+
+# %%
+
+
+
+
+
+#!REPRENDRE A 4.3.1 LE COURS
