360bec0e7b2ea0d820d0122f5efe10bb718e9e10
keniour <el.roussarie@gmail.com>
2024-04-01 01:20:18 +0200

    x

--- IMDB-review_dataset.py
+++ IMDB-review_dataset.py
@@ -214,6 +214,108 @@ plt.show
 
 
 
+
+
+#%% 4.3.1
+
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+from keras import regularizers
+
+(train_data, train_labels),(test_data,test_labels)=imdb.load_data(num_words=10000)
+def vectorize_sequences(sequences,dimension=10000):
+    results=np.zeros((len(sequences),dimension))
+    for i, sequence in enumerate(sequences):
+        results[i][sequence]=1.
+    return results
+x_train = vectorize_sequences(train_data)
+x_test=vectorize_sequences(test_data)
+#vectorisation des labels
+y_train=np.asarray(train_labels).astype('float32')
+y_test=np.asarray(test_labels).astype('float32')
+
+'''
+model=models.Sequential()
+model.add(layers.Dense(16, kernel_regularizer=regularizers.L1(l1=0.001), #!weight regularization
+                       activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16, kernel_regularizer=regularizers.L1(l1=0.001),
+                       activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+'''
+model=models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dropout(0.5)) #! Ajout de Dropout à 50%
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+#Compiling the model with an optimizer and a loss function
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+#Validating the approach
+x_val=x_train[:10000]
+partial_x_train=x_train[10000:]
+y_val=y_train[:10000]
+partial_y_train=y_train[10000:]
+
+
+history=model.fit(partial_x_train,
+                  partial_y_train,
+                  epochs=20,
+                  batch_size=512,
+                  validation_data=(x_val,y_val))
+results=model.evaluate(x_test,y_test)
+print(results)
+
+
+
+#lotting the training and validation loss
+import matplotlib.pyplot as plt
+
+history_dict=history.history
+loss_values=history_dict['loss']
+val_loss_values=history_dict['val_loss']
+acc=history_dict['accuracy']
+
+epochs=range(1,len(acc)+1)
+
+plt.plot(epochs,loss_values,'bo',label='Training loss')
+plt.plot(epochs,val_loss_values,'b',label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+
+#plotting the training and validation accuracy
+
+plt.clf()
+acc_values=history_dict['accuracy']
+val_acc_values=history_dict["val_accuracy"]
+
+plt.plot(epochs, acc_values, 'bo',label='Training acc')
+plt.plot(epochs,val_acc_values,'b',label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show
+
+
+
+
+
 # %%Reverse review
 word_index = imdb.get_word_index()
 reverse_word_index=dict(
